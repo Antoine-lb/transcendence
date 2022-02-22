19c33e3a6271cea2805e1a46b637870ad35ab12b
voltaire-stn <tsantoni@lilo.org>
2022-02-22 01:17:50 +0100

    upload + Guard + save to file system

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { UsersService } from './users.service';
 import { UserEntity } from './users.entity';
 
-import { Controller, Get, Req, UseGuards, Post, Param, Res, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards, Post, Param, Res, UseInterceptors, UploadedFile, Request } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import Jwt2FAGuard from 'src/auth/jwt2FA.guard';
@@ -22,7 +22,6 @@ export const storage = {
           cb(null, `${filename}${extension}`)
       }
   })
-
 }
 
 @Controller('users')
@@ -42,10 +41,12 @@ export class UserController {
       return await this.userService.findAll(); //TODO: voir si je peux return que les infos public
     }
 
+    @UseGuards(JwtAuthGuard, Jwt2FAGuard)
     @Post('upload') // 'file' = valeur de la variable a envoyer dans le POST
     @UseInterceptors(FileInterceptor('file', storage))
-    uploadFile(@UploadedFile() file): Observable<Object> {
-      console.log(file);
+    uploadFile(@UploadedFile() file, @Request() req): Observable<Object> {
+      const user: User = req.user;
+      console.log(user);
       return of({imagePath: file.filename}) // of = observable
     }
     // @UseGuards(JwtAuthGuard, Jwt2FAGuard)
