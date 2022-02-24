0ca6118dd30ed71ccc1c041464e783dccf2ba8bc
ovoda <cdetourrispro@gmail.com>
2022-02-24 16:36:39 +0100

    http fix

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -1,72 +1,26 @@
 <template>
-<main>
-  <div class="about">
-    <form class="form-group">
+  <main>
+    <div class="about">
+      <form class="form-group">
+        <input type="checkbox" id="switch" v-on:click="toggleTwoFA" />
+        <!-- <div style="display=flex">         -->
+        Would you like to enable 2FA
+        <label for="switch">Toggle</label>
+        <!-- </div> -->
+      </form>
 
-    <input 
-      type="checkbox"
-      id="switch" v-on:click="toggleTwoFA" /> 
-    <!-- <div style="display=flex">         -->
-      Would you like to enable 2FA
-      <label for="switch">Toggle</label>
-    <!-- </div> -->
-    </form>
-
-    <form class="form-group">
-      <h2>LogIn</h2>
-      <input
-        v-model="emailLogin"
-        type="email"
-        class="form-control"
-        placeholder="Email"
-        required
-      />
-      <input
-        v-model="passwordLogin"
-        type="password"
-        class="form-control"
-        placeholder="Password"
-        required
-      />
-      <input type="submit" class="submit" @click="doLogin" />
-    </form>
-    <form class="form-group">
-      <h2>Créer un compte</h2>
-      <input
-        v-model="emailLogin"
-        type="name"
-        class="form-control"
-        placeholder="Name"
-        required
-      />
-      <input
-        v-model="emailLogin"
-        type="email"
-        class="form-control"
-        placeholder="Email"
-        required
-      />
-      <input
-        v-model="passwordLogin"
-        type="password"
-        class="form-control"
-        placeholder="Password"
-        required
-      />
-      <input type="submit" class="submit" @click="doLogin" />
-    </form>
-    <a class="intra-login" href="localhost:3000/api/auth/login">
-      <div class="intra-login-wrapper">
-        <p>Se connecter avec</p>
-        <img
-          alt="Invader Logo"
-          class="logo-42"
-          src="@/assets/logo-42-black.png"
-        />
-      </div>
-    </a>
-  </div>
-</main>
+      <a class="intra-login" href="http://localhost:3000/api/auth/login">
+        <div class="intra-login-wrapper">
+          <p>Se connecter avec</p>
+          <img
+            alt="Invader Logo"
+            class="logo-42"
+            src="@/assets/logo-42-black.png"
+          />
+        </div>
+      </a>
+    </div>
+  </main>
 </template>
 
 <style>
@@ -115,85 +69,82 @@
 }
 
 /* POUR LA TICK BOX */
-input[type=checkbox]{
-	height: 0;
-	width: 0;
-	visibility: hidden;
+input[type="checkbox"] {
+  height: 0;
+  width: 0;
+  visibility: hidden;
 }
 
 label {
-	cursor: pointer;
-	/* text-indent: -9999px; */
-	text-indent: 115px;
-	/* width: 200px; origin */
-	width: 20%;
-	/* height: 100px; origin */
-	/* height: 20%; */
-	background: grey;
-	/* display: block; */
-	border-radius: 100px;
-	position: relative;
+  cursor: pointer;
+  /* text-indent: -9999px; */
+  text-indent: 115px;
+  /* width: 200px; origin */
+  width: 20%;
+  /* height: 100px; origin */
+  /* height: 20%; */
+  background: grey;
+  /* display: block; */
+  border-radius: 100px;
+  position: relative;
 }
 
 label:after {
-	content: '';
-	position: absolute;
-	top: 5px;
-	left: 5px;
-	/* width: 90px; origin */
-	width: 35%;
-	/* height: 90px; origin */
-	height: 60%;
-	background: #fff;
-	border-radius: 90px;
-	transition: 0.3s;
+  content: "";
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  /* width: 90px; origin */
+  width: 35%;
+  /* height: 90px; origin */
+  height: 60%;
+  background: #fff;
+  border-radius: 90px;
+  transition: 0.3s;
 }
 
 input:checked + label {
-	background: #bada55;
+  background: #bada55;
 }
 
 input:checked + label:after {
-	left: calc(100% - 5px);
-	transform: translateX(-100%);
+  left: calc(100% - 5px);
+  transform: translateX(-100%);
 }
 
 label:active:after {
-	/* width: 130px; origin */
-	width: 5%;
+  /* width: 130px; origin */
+  width: 5%;
 }
 
 /*  centering */
 body {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	/* height: 100vh; origin*/
-	height: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* height: 100vh; origin*/
+  height: 5%;
 }
 </style>
 
 <script lang="ts">
-
 export default {
   // name : "Game",
-  data () {
+  data() {
     return {
       twoFA: false,
-    }
+    };
   },
   methods: {
-    toggleTwoFA () {
+    toggleTwoFA() {
       this.twoFA = !this.twoFA;
       // if (this.twoFA)
-        // ASK BACKEND QRcode
+      // ASK BACKEND QRcode
       console.log(this.twoFA);
-    }
-  }
+    },
+  },
   // components : {
   //   TheGame
   // }
-
 };
-
 </script>
