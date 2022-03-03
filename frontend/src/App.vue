6995187595770829861c3b83848d99c8547f548c
Antoine-LB <antoine.stc25@gmail.com>
2022-03-03 14:29:11 +0100

    remouve account

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,10 +14,8 @@ import HelloWorld from "@/components/HelloWorld.vue";
     <div class="wrapper">
       <nav class="bg-pan-left">
         <RouterLink to="/">Accueil</RouterLink>
-        <RouterLink to="/compte">Compte</RouterLink>
-        <!-- <RouterLink to="/compte">Classement</RouterLink> -->
-        <RouterLink to="/game">Jeux</RouterLink>
         <RouterLink to="/chat">Chat</RouterLink>
+        <RouterLink to="/game">Jeux</RouterLink>
       </nav>
     </div>
   </header>
