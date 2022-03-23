6d9ca648c01e40bf24944b874720589dd7d2dd7d
Antoine-LB <antoine.stc25@gmail.com>
2022-03-23 14:44:25 +0100

    style chat  done

--- frontend/src/components/ChatNewRoom.vue
+++ frontend/src/components/ChatNewRoom.vue
@@ -81,10 +81,10 @@ export default {
       </li>
       <div>
         <input type="text" v-model="newRoomUser" placeholder="Room Users" />
-        <button @click="newUser">+</button>
+        <button class="add-user" @click="newUser">Add user</button>
       </div>
       <p v-if="newRoomUserShowError" class="error-paragraf">
-        "{{ newRoomUser }}" username not found
+        Username not found
       </p>
       <!-- <input class="button" type="submit" value="Ajouter" /> -->
       <button class="submit-new-room" @click="createRooms">
@@ -108,7 +108,7 @@ input {
   font-size: 18px;
 }
 
-button {
+.submit-new-room {
   background-color: #703ab8;
   border: none;
   color: white;
@@ -121,7 +121,7 @@ button {
   margin-top: 10px;
   /* display: block; */
 }
-button:hover {
+.submit-new-room:hover {
   background-color: white;
   color: #703ab8;
 }
@@ -146,5 +146,26 @@ button:hover {
   width: 100%;
   border-radius: 3px;
   font-size: 18px;
+}
+
+.add-user {
+  background-color: white;
+  border: none;
+  color: #703ab8;
+  font-weight: bold;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 3px;
+  padding: 6px 15px;
+  cursor: pointer;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  margin-top: 10px;
+  /* display: block; */
+  margin: 10px;
+  border: 2px solid #703ab8;
+}
+
+.add-user:hover {
+  background-color: #703ab8;
+  color: white;
 }
 </style>
