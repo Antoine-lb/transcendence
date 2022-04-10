306186a3488e8dc882cf54e24978d6f99e60f26d
smiley16479 <smiley16479@aol.com>
2022-04-10 04:32:18 +0200

    🙏 yes it will work

--- backend/src/gamee/gateway/game.gateway.ts
+++ backend/src/gamee/gateway/game.gateway.ts
@@ -1,38 +1,52 @@
 import {
   SubscribeMessage,
   WebSocketGateway,
+  OnGatewayInit,
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
- } from '@nestjs/websockets';
-import { UnauthorizedException } from '@nestjs/common';
+} from '@nestjs/websockets';
+import { Logger, UnauthorizedException } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 import { AuthService } from 'src/auth/auth.service';
 import { UsersService } from 'src/users/users.service';
+import { runInThisContext } from 'vm';
+import { UserDto } from 'src/entities/users.dto';
+import { comparePassword } from 'src/utils/bcrypt';
 import { GameService } from 'src/gamee/service/game/game.service';
 import { FRAME_RATE, maxPaddleY, grid, canvas, paddleHeight, } from 'src/utils/constants';
+import { RoomEntity } from 'src/chat/model/room.entity';
+import { RoomI } from 'src/chat/model/room.interface';
 import { StateI } from 'src/gamee/model/state.interface';
 
- 
 
- @WebSocketGateway({
-   cors: {
-     origin: '*',
-   },
- })
-export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
- 
-   constructor(
-     private authService: AuthService,
-     private userService: UsersService,
-     private GameService: GameService,
-    ) { }
-  
-   
-   
-   @WebSocketServer() server: Server;
-   state: StateI[] = [];
-   
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+    private GameService: GameService,
+  ) { }
+
+
+
+  @WebSocketServer() server: Server;
+
+  intervalId: NodeJS.Timer;
+
+  state: StateI[] = [];
+
+
+
+  async onModuleInit() {
+  }
+
   async handleConnection(socket: Socket, payload: string) {
     try {
       const decodedToken = await this.authService.verifyToken(socket.handshake.headers.authorization);
@@ -44,7 +58,6 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
       else {
         socket.data.user = user;
         socket.data.number = null;
-        socket.data.roomId = 0;
         return;
       }
     }
@@ -53,15 +66,19 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
       return this.disconnect(socket);
     }
   }
-   
+
   @SubscribeMessage('joinGame')
-   handleJoinGame(socket: Socket, roomName: string) {
+  handleJoinGame(socket: Socket, roomName: string) {
 
     let roomSize = 0;
-  	const room = this.server.sockets.adapter.rooms.get(roomName)
+    console.log('------------after Join------------------------')
+
+    console.log(this.state);
+    const room = this.server.sockets.adapter.rooms.get(roomName)
 
     if (room)
       roomSize = this.server.sockets.adapter.rooms.get(roomName).size;
+    console.log("room", room)
 
     if (roomSize === 0) {
       socket.emit('unknownCode');
@@ -73,86 +90,52 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     let roomNameINT: number = parseInt(roomName);
     let index: number = this.GameService.getRoomById(this.state, roomNameINT);
-  
+
+
     this.state[index].id = roomNameINT;
     // set the creator to player 1
     socket.data.number = 2;
     // set the room game id to the current user socket
     socket.data.roomId = roomName;
+
     // join the room socket
     socket.join(roomName);
+
+
     // init the front for player 2
     socket.emit('init', 2);
+
     // start the game when both player are connected
     this.startGameInterval(roomNameINT);
   }
-   
-   
-   
-   @SubscribeMessage('joinQueue')
-   handleJoinQueue(socket: Socket) {
 
-    let index: number = this.GameService.getRoomForQueue(this.state);
-
-     // [CREATE] game state and wait other player if(nobody is in queue)
-     if (index == -1) {
-      // create random ID for the new room
-      let roomName = this.GameService.makeid(5);
-      // init the game state 
-      let newState: StateI = this.GameService.initGame(true)
-      newState.gameState = "play";
-      newState.status = 1;
-      newState.id = parseInt(roomName);
-      // save the new game state
-      this.state.push(newState);
-      // set the creator to player 1
-      socket.data.number = 1;
-      // set the room game id to the current user socket
-      socket.data.roomId = roomName;
-      // join the room socket
-      socket.join(roomName);
-
-      // init the front for player 1
-      socket.emit('init', 1);
-     }
-     // [JOIN] the game if somebody is already in queue
-     else {
-      // set the creator to player 1
-      socket.data.number = 2;
-      // set the room game id to the current user socket
-      socket.data.roomId = this.state[index].id;
-      // join the room socket
-      socket.join(this.state[index].id.toString());
-      // init the front for player 2
-      socket.emit('init', 2);
-      // start the game when both player are connected
-      this.startGameInterval(this.state[index].id);
-    }
- }
-   
   @SubscribeMessage('newGame')
   handleNewGame(socket: Socket) {
-    
-    // create random ID for the new room
+
     let roomName = this.GameService.makeid(5);
-    // emit the new game ID to other player;
+
+    // clientRooms[socket.id] = roomName;
     socket.emit('gameCode', roomName);
-    // init the game state 
-    let newState: StateI =  this.GameService.initGame(false)
+
+    let newState: StateI = this.GameService.initGame()
+
     newState.gameState = "play";
     newState.id = parseInt(roomName);
-    // save the new game state
+
     this.state.push(newState);
+
     // set the creator to player 1
     socket.data.number = 1;
     // set the room game id to the current user socket
     socket.data.roomId = roomName;
+
     // join the room socket
     socket.join(roomName);
+
     // init the front for player 1
     socket.emit('init', 1);
   }
-     
+
   // @SubscribeMessage('pause')
   // async handlePause(socket: Socket) {
   //   // let room = this.state[clientRooms[socket.id]];
@@ -174,85 +157,101 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   //     this.startGameInterval(this.state.id);
   // 	}
   // }
-   
+
+  @SubscribeMessage('disconnect')
+  async handleDisconnection(socket: Socket) {
+    console.log("socket :", socket.id, " juts disconnected")
+    if (socket.data.roomId && this.state[parseInt(socket.data.roomId)].intervalId)
+      clearInterval(this.state[socket.data.roomId].intervalId);
+    // socket.broadcast.emit('disconnection')
+  }
+
+  //  (/)
   @SubscribeMessage('keydown')
- handleKeyDown(socket: Socket, keyCode: string) {
+  async handleKeyDown(socket: Socket, keyCode: string) {
 
     let keyCodeInt: number;
-    let index: number = this.GameService.getRoomById(this.state,  socket.data.roomId);
+    let index: number = await this.GameService.getRoomById(this.state, socket.data.roomId);
     if (!this.state[index]) {
       return;
     }
     try {
       keyCodeInt = parseInt(keyCode);
-    } catch(e) {
+    } catch (e) {
       console.error(e);
       return;
     }
 
-    const vel = this.GameService.getUpdatedVelocity(keyCodeInt);
-  
-    if (vel) {
-      this.state[index].players[socket.data.number - 1].y += vel;
+    const vel = this.GameService.getUpdatedVelocity(false, keyCodeInt, this.state[index].players[socket.data.number - 1]);
+  }
+  //  (/)
+  @SubscribeMessage('keyup')
+  async handleKeyup(socket: Socket, keyCode: string) {
+    console.log("key Up")
+
+    let index: number = await this.GameService.getRoomById(this.state, socket.data.roomId);
+    if (!this.state[index]) {
+      return;
     }
-    // prevent paddles from going through walls
-    if ( this.state[index].players[socket.data.number - 1].y < grid) {
-      this.state[index].players[socket.data.number - 1].y = grid;
-    }
-    else if ( this.state[index].players[socket.data.number - 1].y > maxPaddleY) {
-      this.state[index].players[socket.data.number - 1].y = maxPaddleY;
-    }
+
+    this.GameService.getUpdatedVelocity(true, keyCode, this.state[index].players[socket.data.number - 1]);
   }
 
-   async handleDisconnect(socket: Socket) {
-    
+
+  async handleDisconnect(socket: Socket) {
+
     // remove client to connected repository
     socket.disconnect()
   }
 
-   private disconnect(socket: Socket) {
-    
+  private disconnect(socket: Socket) {
+
     socket.emit('Error', new UnauthorizedException());
     socket.disconnect();
   }
-   
+
+  async afterInit() { }
+
   startGameInterval(roomId: number) {
-      
-    let index: number = this.GameService.getRoomById(this.state, roomId);
-     
-    this.state[index].intervalId = setInterval(() =>  {
-        let index: number = this.GameService.getRoomById(this.state, roomId);
-          
-        const winner = this.GameService.gameLoop(this.state[index]);
-            
-        if (!winner) {
-          this.emitGameState(this.state, roomId);
-        }
-        else {
-          this.emitGameOver(this.state, winner, roomId);
 
-            // TODO : save the score
-          clearInterval(this.state[index].intervalId);
-          this.state.splice(index, 1);
+    this.state[roomId].intervalId = setInterval(() => {
 
-        }
+      let index: number = this.GameService.getRoomById(this.state, roomId);
+
+      const winner = this.GameService.gameLoop(this.state[index]);
+
+      if (!winner) {
+        this.emitGameState(this.state, roomId);
+      }
+      else {
+        clearInterval(this.intervalId);
+        this.emitGameOver(this.state[index], winner);
+
+        // TODO : save the score
+        this.state.splice(index, 1);
+
+        console.log('------------after SPLIRCE------------------------');
+        console.log(index);
+
+        console.log(this.state);
+      }
     }, 1000 / FRAME_RATE);
   }
-   
+
   emitGameState(state: StateI[], roomId: number) {
 
     let index: number = this.GameService.getRoomById(this.state, roomId);
-     
+
+    console.log(index);
+
     // Send this event to everyone in the room.
     this.server.to(state[index].id.toString())
-    .emit('gameState', JSON.stringify(state[index]));
+      .emit('gameState', JSON.stringify(state[index]));
   }
-    
-  emitGameOver(state: StateI[], winner: number, roomId: number) {
 
-    let index: number = this.GameService.getRoomById(this.state, roomId);
+  emitGameOver(state: StateI, winner) {
 
-    this.server.to(state[index].id.toString())
-    .emit('gameOver', JSON.stringify({ winner }));
-   }
+    this.server.to(state.id.toString())
+      .emit('gameOver', JSON.stringify({ winner }));
+  }
 }
--- frontend/src/components/TheGame.vue
+++ frontend/src/components/TheGame.vue
@@ -81,57 +81,92 @@ export default {
     });
   },
   methods: {
-   init() {
+    socketSetter() {
+      this.socket = io("http://127.0.0.1:3000", {
+      extraHeaders: {
+        Authorization: this.user.access_token,
+      },
+      });
+      this.socket.on("connect", () => {this.gameStatus = "idle"})
+      this.socket.on("init", this.handleInit);
+      this.socket.on("gameState", this.handleGameState);
+      this.socket.on("gameOver", this.handleGameOver);
+      this.socket.on("gameCode", this.handleGameCode);
+      this.socket.on("unknownCode", this.handleUnknownCode);
+      this.socket.on("tooManyPlayers", this.handleTooManyPlayers);
+      this.socket.on("paused", this.handlePause);
+      this.socket.on("notify", this.handleNotification);
+      this.socket.on("disconnection", this.handleDisconnection);
+      this.socket.on("disconnect", (reason) => {
+        if (reason === "io server disconnect") {
+          // the disconnection was initiated by the server, you need to reconnect manually
+          this.socket.connect();
+        }
+      // else the socket will automatically try to reconnect
+      });
+    },
+
+    createNewGame() {
+      console.log("new game");
+
+      this.socketSetter();
+      // console.log("this.socket", this.socket.emit("createGame"));
+      this.socket.emit("newGame"/* "createGame" */);
+      this.init();
+    },
+
+    handleJoinGame() {
+      const code = this.gameCodeInput.value;
+      this.socketSetter();
+      this.socket.emit('joinGame', code);
+      this.init();
+    },
+
+    init() {
       this.initialScreen.style.display = "none";
       this.gameScreen.style.display = "block";
-
-      // canvas = document.getElementById('canvas');
-      // this.canvas = this.$refs.game;
       this.ctx = this.canvas.getContext("2d");
       this.canvas.width = 750;
-      this.canvas.height = 585;
-
+      this.canvas.height = 590;
       this.ctx.fillStyle = "#231f20";
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
       document.addEventListener("keydown", this.keydown);
+      document.addEventListener("keyup", this.keyup);
       this.gameActive = true;
     },
 
     keydown(e) {
       // console.log(e.key)
-      console.log(e.keyCode)
+      // console.log(e.keyCode)
+      if (!this.socket.connected)
+        return;
       this.socket.emit("keydown", e.keyCode);
       if (e.key == 'd')
-        this.socket.disconnect()
+        this.socket.disconnect();
       if (e.key == 'f')
-        this.socket.connect()
+        this.socket.connect();
+    },
+
+    keyup(e) {
+      if (!this.socket.connected)
+        return;
+      this.socket.emit("keyup", e.keyCode);
     },
 
     paintGame(state) {
       const grid = 15;
-      const paddleHeight = grid * 5
       // clear canvas
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       // draw paddles
       this.ctx.fillStyle = "black";
-      this.ctx.fillRect(grid, state.players[0].y, 15, paddleHeight);
-      this.ctx.fillRect(
-        this.canvas.width - 2 * grid,
-        state.players[1].y,
-        15,
-        paddleHeight
-      );
+      this.ctx.fillRect(grid, state.players[0].y, 15, state.players[0].paddleH);
+      this.ctx.fillRect(this.canvas.width - 2 * grid,state.players[1].y, 15, state.players[1].paddleH);
 
       // draw walls
       this.ctx.fillStyle = "lightgrey";
       this.ctx.fillRect(0, 0, this.canvas.width, grid);
-      this.ctx.fillRect(
-        0,
-        this.canvas.height - grid,
-        this.canvas.width,
-        this.canvas.height
-      );
+      this.ctx.fillRect( 0, this.canvas.height - grid, this.canvas.width, this.canvas.height);
 
       // draw dotted line down the middle
       for (
@@ -151,7 +186,7 @@ export default {
 
       // draw PowerUps (if any)
       if (state.powerUp[0].x > 0) {
-        console.log("state.powerUp_t", state.powerUp_t)
+        // console.log("state.powerUp_t", state.powerUp_t)
         this.ctx.fillStyle = state.powerUp_t;
         this.ctx.fillRect(state.powerUp[0].x, state.powerUp[0].y, 15, 15);
         this.ctx.fillRect(state.powerUp[1].x, state.powerUp[1].y, 15, 15);
@@ -174,7 +209,7 @@ export default {
       }
       gameState = JSON.parse(gameState);
       this.score = gameState.score;
-      console.log("this.gameStatus : ", this.gameStatus);
+      // console.log("this.gameStatus : ", this.gameStatus);
       if (this.gameStatus !== "opponentLeft" && this.gameStatus !== "paused")
         requestAnimationFrame(() => this.paintGame(gameState));
     },
@@ -183,8 +218,10 @@ export default {
       if (!this.gameActive) {
         return;
       }
-      data = JSON.parse(data);
+      document.removeEventListener("keydown", this.keydown);
+      document.removeEventListener("keyup", this.keydown);
 
+      data = JSON.parse(data);
       this.gameActive = false;
 
       if (data.winner === this.playerNumber) {
@@ -244,19 +281,6 @@ export default {
       this.initialScreen.style.display = "block";
       this.gameScreen.style.display = "none";
     },
-
-    handleJoinGame() {
-      const code = this.gameCodeInput.value;
-      this.socket.emit('joinGame', code);
-      this.init();
-    },
-
-    createNewGame() {
-      console.log("new game");
-      // console.log("this.socket", this.socket.emit("createGame"));
-      this.socket.emit("newGame"/* "createGame" */);
-      this.init();
-    },
   },
 };
 </script>
@@ -268,7 +292,7 @@ export default {
       <div ref="initialScreen" class="h-100">
         <div class="d-flex flex-column align-items-center justify-content-center h-100">
             <h1>Multiplayer Pong Game</h1>
-            <h1>myRoom is : {{this.socket.id}}</h1>
+            <h1>myRoom is : {{this.socket != null ? this.socket.id : "Undefined yet"}}</h1>
             <button
               type="submit"
               class="btn btn-success"
@@ -292,7 +316,7 @@ export default {
 
       <div ref="gameScreen" id="gameScreen" class="h-100">
         <div class="d-flex flex-column align-items-center justify-content-center h-100">
-          <h1>myRoom is : {{this.socket.id}}</h1>
+          <h1>myRoom is : {{this.socket != null ? this.socket.id : "Undefined yet"}}</h1>
           <h1>Your game code is: <span ref="gameCodeDisplay"></span></h1>
           <canvas ref="canvas"></canvas>
           <button
