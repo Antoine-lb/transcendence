ce9dd6c8711d3cd2963f268389ea06bcee2cfba6
Antoine-LB <antoine.stc25@gmail.com>
2022-05-11 15:14:52 +0200

    some styleing (for the 3rd time)

--- frontend/src/components/TheGame.vue
+++ frontend/src/components/TheGame.vue
@@ -14,8 +14,6 @@ export default {
     const msgBox = ref(null);
     const canvas = ref(null);
 
-
-
     return {
       initialScreen,
       gameCodeInput,
@@ -26,7 +24,6 @@ export default {
       userStore,
     };
   },
-
 
   props: {
     user: Object,
@@ -49,11 +46,11 @@ export default {
   },
   mounted() {
     console.log(`mounted`);
-    this.socket.emit('check_on_game');
+    this.socket.emit("check_on_game");
     this.reset();
   },
   unmounted() {
-    this.socket.emit('test');
+    this.socket.emit("test");
     this.socket.removeAllListeners();
   },
 
@@ -138,7 +135,7 @@ export default {
       this.ctx = this.canvas.getContext("2d");
       this.canvas.width = 750;
       this.canvas.height = 590;
-      this.ctx.fillStyle = "#231f20";
+      this.ctx.fillStyle = "#703ab8";
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
       document.addEventListener("keydown", this.keydown);
@@ -171,9 +168,8 @@ export default {
           );
           // await this.sleep(10);
         }
-      this.gameStatus = "play"
-      if (this.playerNumber == 1)
-        this.socket.emit("startGame")
+      this.gameStatus = "play";
+      if (this.playerNumber == 1) this.socket.emit("startGame");
     },
 
     sleep(ms) {
@@ -181,13 +177,12 @@ export default {
     },
 
     paintGame(state) {
-      if (!this.canvas)
-        return ;
+      if (!this.canvas) return;
       const grid = 15;
       // clear canvas
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       // draw paddles
-      this.ctx.fillStyle = "black";
+      this.ctx.fillStyle = "#703ab8";
       this.ctx.fillRect(grid, state.players[0].y, 15, state.players[0].paddleH);
       this.ctx.fillRect(
         this.canvas.width - 2 * grid,
@@ -197,7 +192,7 @@ export default {
       );
 
       // draw walls
-      this.ctx.fillStyle = "lightgrey";
+      this.ctx.fillStyle = "#e4d8f5";
       this.ctx.fillRect(0, 0, this.canvas.width, grid);
       this.ctx.fillRect(
         0,
@@ -265,7 +260,7 @@ export default {
         requestAnimationFrame(() => this.paintGame(gameState));
     },
 
-/*     handleGameOver(data) {
+    /*     handleGameOver(data) {
       if (!this.gameActive) {
         return;
       }
@@ -296,17 +291,17 @@ export default {
       if (!this.gameActive) {
         return;
       }
-      this.gameStatus == "ended"
+      this.gameStatus == "ended";
       document.removeEventListener("keydown", this.keydown);
       document.removeEventListener("keyup", this.keydown);
 
       this.gameActive = false;
       this.$notify({
-          position: "center",
-          title: "The Game has reach its end..",
-          text: data + " has Won !!",
-          duration: 6000,
-        });
+        position: "center",
+        title: "The Game has reach its end..",
+        text: data + " has Won !!",
+        duration: 6000,
+      });
     },
 
     handleUnknownCode() {
@@ -335,7 +330,7 @@ export default {
         "background: #222; color: #bada55"
       );
       this.socket.emit("pause");
-      this.gameStatus = this.gameStatus == "paused" ? 'play' : 'paused'
+      this.gameStatus = this.gameStatus == "paused" ? "play" : "paused";
     },
 
     handleNotification(msg) {
@@ -343,7 +338,10 @@ export default {
     },
 
     reset() {
-      this.gameStatus = (this.gameStatus == "play" || this.gameStatus == "paused" ? this.gameStatus : "idle");
+      this.gameStatus =
+        this.gameStatus == "play" || this.gameStatus == "paused"
+          ? this.gameStatus
+          : "idle";
       this.playerNumber = null;
       if (this.gameCodeInput) this.gameCodeInput.value = "";
       if (this.gameCodeSpec) this.gameCodeSpec.value = "";
@@ -359,7 +357,7 @@ export default {
     pushLiveGame(liveGame) {
       console.log("ds pushLiveGame");
       console.log(liveGame);
-      this.gameStatus = "play"
+      this.gameStatus = "play";
     },
   },
 };
@@ -404,15 +402,15 @@ export default {
             myRoom is :
             {{ this.socket != null ? this.socket.id : "Undefined yet" }}
           </h1>
-          <canvas ref="canvas"></canvas>
+          <canvas ref="canvas" class="game-canvas"></canvas>
           <button
-            v-if="this.gameStatus == 'play' ||  this.gameStatus == 'paused'"
+            v-if="this.gameStatus == 'play' || this.gameStatus == 'paused'"
             type="submit"
             class="btn btn-success"
             ref="pauseButton"
             v-on:click="handlePause()"
           >
-            {{ this.gameStatus ==  'play' ? 'pause' : 'continue'}} 
+            {{ this.gameStatus == "play" ? "pause" : "continue" }}
           </button>
           <button
             type="submit"
@@ -456,6 +454,15 @@ export default {
 </template>
 
 <style scoped>
+.game-canvas {
+  margin: auto;
+  width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 0 6px rgba(120, 61, 204, 0.92), 0 0 30px rgba(94, 14, 206, 0.34),
+    0 0 12px rgba(211, 193, 236, 0.52), 0 0 21px rgba(211, 193, 236, 0.92),
+    0 0 34px rgba(211, 193, 236, 0.78), 0 0 54px rgba(211, 193, 236, 0.92); /* box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23); */
+}
+
 #gameScreen {
   display: none;
 }
