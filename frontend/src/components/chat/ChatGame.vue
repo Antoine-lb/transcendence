6a87a7b04b61c9192e5d2dc4de614372dce5ecbf
Antoine-LB <antoine.stc25@gmail.com>
2022-05-14 15:20:27 +0200

    hide game chat

--- frontend/src/components/chat/ChatGame.vue
+++ frontend/src/components/chat/ChatGame.vue
@@ -308,8 +308,7 @@ export default {
     },
 
     reset() {
-      if (!this.hasBeenInvited)
-      {
+      if (!this.hasBeenInvited) {
         this.socket.emit("test");
       }
       this.gameStatus =
@@ -331,13 +330,8 @@ export default {
     <div class="container h-100">
       <div style="margin-bottom: 30px">
         <h1>Invite your mates to play pong below</h1>
-        <div
-          v-for="(game, index) in this.liveGame"
-          :key="game.liveGame"
-          v-on:click="handleSpecGame(index)"
-          :title="index"
-          class="list"
-        >
+        <div v-for="(game, index) in this.liveGame" :key="game.liveGame" v-on:click="handleSpecGame(index)"
+          :title="index" class="list">
           <span v-if="this.gameStatus !== 'playing'">
             👾 Join <strong>{{ game.player1 }}</strong> and
             <strong>{{ game.player2 }}</strong> game
@@ -346,25 +340,17 @@ export default {
       </div>
       <div class="h-100">
         <div class="" style="display: flex; flex-direction: column">
-          <canvas ref="canvas" class="game-canvas"></canvas>
+          <canvas ref="canvas" class="game-canvas" v-bind:class="{
+            'display-none': this.gameStatus == 'idle',
+          }"></canvas>
 
-          <button
-            v-if="this.gameStatus !== 'idle'"
-            type="submit"
-            class="btn btn-success"
-            ref="pauseButton"
-            v-on:click="handlePause"
-          >
+          <button v-if="this.gameStatus !== 'idle'" type="submit" class="btn btn-success" ref="pauseButton"
+            v-on:click="handlePause">
             ⏸ Pause {{ this.gameStatus }}
           </button>
 
-          <button
-            v-if="this.gameStatus === 'opponentLeft'"
-            type="submit"
-            class="btn btn-success"
-            ref="notifyButton"
-            v-on:click="claimVictory"
-          >
+          <button v-if="this.gameStatus === 'opponentLeft'" type="submit" class="btn btn-success" ref="notifyButton"
+            v-on:click="claimVictory">
             claimVictory
           </button>
         </div>
@@ -380,7 +366,8 @@ export default {
   border-radius: 10px;
   box-shadow: 0 0 6px rgba(120, 61, 204, 0.92), 0 0 30px rgba(94, 14, 206, 0.34),
     0 0 12px rgba(211, 193, 236, 0.52), 0 0 21px rgba(211, 193, 236, 0.92),
-    0 0 34px rgba(211, 193, 236, 0.78), 0 0 54px rgba(211, 193, 236, 0.92); /* box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23); */
+    0 0 34px rgba(211, 193, 236, 0.78), 0 0 54px rgba(211, 193, 236, 0.92);
+  /* box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23); */
 }
 
 .btn {
@@ -412,5 +399,9 @@ strong {
   /* font-size: 40px; */
   text-transform: capitalize;
   font-weight: 600;
+}
+
+.display-none {
+  height: 0px;
 }
 </style>
